f29d44ad9eba5a520354cfb15cfee5b9db36fc5c
Andres Weber <andresmweber@gmail.com>
2020-06-30 10:38:18 -0400

    Added basic setup.  Triggering build.

---
+++ lambdas/utils/apiResponses.js
@@ -0,0 +1,31 @@
+const CommonHeaders = {
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Methods": "*",
+  "Access-Control-Allow-Origin": "*"
+};
+
+const Responses = {
+  _200(data = {}) {
+    return {
+      headers: { ...CommonHeaders },
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  },
+  _404(data = {}) {
+    return {
+      headers: { ...CommonHeaders },
+      statusCode: 404,
+      body: JSON.stringify(data)
+    };
+  },
+  _400(data = {}) {
+    return {
+      headers: { ...CommonHeaders },
+      statusCode: 400,
+      body: JSON.stringify(data)
+    };
+  }
+};
+
+module.exports = Responses;
