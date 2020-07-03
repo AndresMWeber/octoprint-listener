9ba1dbf9cb238e7be27e9d400b701166ce16e57f
Andres Weber <andresmweber@gmail.com>
2020-07-03 13:37:36 -0400

    Added TAG setup, fixed SLS env var defaults, added .coverage folder specification.

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 
-const IMAGES_BUCKET = process.env.BUCKET
+const IMAGES_BUCKET = process.env.AW_BUCKET
 const SLACK_WEBHOOK_BASE_URL = `https://hooks.slack.com/services/`
 const SLACK_WEBHOOK_URLS = {
-  octoprint: `${SLACK_WEBHOOK_BASE_URL}${process.env.SLACK_URL}`
+  octoprint: `${SLACK_WEBHOOK_BASE_URL}${process.env.AW_SLACK_URL}`
 }
 
 module.exports = {
